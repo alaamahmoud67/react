32bd670a88b25b291e0fabfac97c08d1217bbc8b
alaamahmoud67 <alamhmwd71@gmail.com>
2024-06-12 17:11:54 +0300

    first commit

--- src/components/Altafsir.jsx
+++ src/components/Altafsir.jsx
@@ -8,9 +8,10 @@ import Aldetails from './Aldetails'
 
 export default function Altafsir() {
     const [first, setFirst] = useState ([])
-    const gett= async () => {
-		const response = await axios.get("http://api.alquran.cloud/v1/meta");
-		setFirst(response.data.data.surahs.references);};
+
+  const gett= async () => {
+	const dataa = await axios.get("http://api.alquran.cloud/v1/meta");
+	setFirst(dataa.data.data.surahs.references);};
 
 
 
@@ -23,25 +24,25 @@ export default function Altafsir() {
      <div className='container my-5'>
       <h1 className='text-center'>القرآن الكريم</h1>
     <div className='row g-3 '>
-    {first.map((t ,index  )=> 
-      <div className='col-lg-3 ' key={index} >
+    {first.map(function(item,index){
+     return(
+    <div className='col-lg-3 ' key={index} >
     
-    <Link to={`/quran/${t.number}`}>
-<div className='card r p-5'   >
-   <p >{t.name}</p>
-   
-   <p >{t.englishName}</p>
+      <Link to={`/quran/${item.number}`}>
+  <div className='card r p-5'   >
+     <p >{item.name}</p>
+     
+     <p >{item.englishName}</p>
+  
+        
+         </div>
+  </Link>
+     
+     </div>
+     )  
+  
 
-      
-       </div>
-</Link>
-   
-   </div>
-
- 
-
-   
-    )}
+})}
     </div>
         </div>
     
